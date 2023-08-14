403f6af1b0b46fad8bd1379f0089803a18375446
김수찬 <tncks097@naver.com>
2023-08-14 23:07:23 +0900

    Feat : contractConsumer api  추가

--- src/backend/chatserver/src/pages/api/contract/constractConsumer.ts
+++ src/backend/chatserver/src/pages/api/contract/constractConsumer.ts
@@ -26,20 +26,12 @@ export const config = {
 };
 
 const receiveData = withCors(async (req: any, res: any) => {
-  // let { uploadToS3 } = useS3Upload();  
-  // console.log(req);
-  // console.log(req.body);
-  // console.log( req.body.producer_sign.get('file'));
-//   for (let [key, value] of req.body.producer_sign.entries()) {
-//     console.log("ㅎㅇ",key, value);
-// }
-
-
-if (req.method === 'POST') {
-
-  await new Promise<void>((resolve, reject) => {
+    
+  //   return
+  if (req.method === 'POST') {
+    await new Promise<void>((resolve, reject) => {
       getData.fields([
-        { name: 'file', maxCount: 1 },
+        { name: 'file', maxCount: 2 },
         { name: 'contractForm', maxCount: 1 }
       ])(req, res, (err: any) => {
           if (err) {
@@ -49,40 +41,35 @@ if (req.method === 'POST') {
           resolve();
       });
     });
-    const {file, contractForm} = req.files;
-    // const inputData = req.contractForm;
-    
-    const image = file[0]
-    let imageUrl = await uploadToS3('signature', image.originalname, image.buffer);
+      const {file, contractForm} = req.files;   
+      console.log(file, contractForm)
+      const image_consumer = file[0]
+      // const image_contract = file[1]
+      // 이미지 upload
+      const consumer_sign = await uploadToS3('signature', image_consumer.originalname, image_consumer.buffer);
+      console.log("consumer_img", consumer_sign)
+      // const contract = await uploadToS3('contract', image_consumer.originalname, image_consumer.buffer);
 
-    const contractFormData = contractForm[0].buffer.toString('utf8');  // buffer를 문자열로 변환
-    const contractFormJSON = JSON.parse(contractFormData);  // 문자열을 JSON으로 파싱
 
-    const {room_id} = contractFormJSON;
-    delete contractFormJSON.room_id;
+      const contractFormData = contractForm[0].buffer.toString('utf8');  // buffer를 문자열로 변환
+      const contractFormJSON = JSON.parse(contractFormData);  // 문자열을 JSON으로 파싱
+      const id = contractFormJSON.id;
 
-    console.log(contractFormJSON);
-    console.log(imageUrl)
-    
-    // // SET 설정
-    const constractFrom = { ...contractFormJSON, producer_sign : imageUrl}
-    
-    // 이미지 upload
-    // Object.assign(newInputData, { producer_sign: url });
-    
-    // 이미지 inputData 처리
-    await createContract(constractFrom);
-    let table = 'History'
-    const [Latest_History] = await dbQuery(`SELECT * FROM ?? WHERE id = LAST_INSERT_ID()`, [table]);
-    const {id} = Latest_History;
-
-    // await updateRoom({contract_id}, room_id);
-    table = 'chat_room';
-    await dbQuery(`UPDATE ?? SET contract_id = ? WHERE id = ?`,[table, id, room_id])
-
-    res.status(200).json({ constractFrom : Latest_History, message: 'send to consumer' });
-    return;
-  }
+    //   const {conditionQuery, values} = buildConditionQuery(id, ' AND ');
+      
+      const NewConstractData = { consumer_sign, contract_path : "example_Path"}
+      
+      
+      const result = await updateContract({...NewConstractData, id});
+      console.log(result);
+      // const contractData = await readQuery('history',{conditionQuery, values})
+      if (!result){
+        res.status(500).json({ message: 'contract failed cuz of server error' });
+        return;
+      }
+      // 이미지 저장하는거 추가해야함
+      res.status(200).json({ contract : "example_Path", message: 'contract finished' })
+    }
     
     if (req.method === 'GET') {
       // 나머지 GET 로직...
